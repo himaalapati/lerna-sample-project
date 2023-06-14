0904d94026e5d9e43f5cd043d61c7cde083ee93f
Hima Bindu <himabindu.alapati1@gmail.com>
2023-06-14 17:28:01 +0530

    initial commit

---
+++ packages/demo-main/lib/demo-main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = demoMain;
+
+function demoMain() {
+  return 'Hello from demoMain';
+}
---
+++ packages/demo-tools/lib/demo-tools.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = demoTools;
+
+function demoTools() {
+  return 'Hello from demoTools';
+}
